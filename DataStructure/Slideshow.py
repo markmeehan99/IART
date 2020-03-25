91fc0f763858f17dbde88298d9465f30f516f52d
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-03-25 01:20:32 +0000

    Made get_initial_state a static method of SlideShow

--- DataStructure/Slideshow.py
+++ DataStructure/Slideshow.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from Slide import Slide
+import random
 
 
 class Slideshow:
@@ -7,8 +8,7 @@ class Slideshow:
     all_ids_set = set()
     vertical_photos_pool = []
 
-    def __init__(self, id, primary_solution):
-        self.id = id
+    def __init__(self, primary_solution):
         self.score = 0
         self.set_slides(primary_solution)
 
@@ -55,3 +55,52 @@ class Slideshow:
                 pass
             self.score += Slide.getScore(self.slides[j], self.slides[i])
             j += 1
+
+    @staticmethod
+    def get_initial_state():
+        n_verticalp = len(Slideshow.vertical_photos_pool)
+        n_horizontalp = len(Slideshow.horizontal_photos_pool)
+        n_max_slides = n_horizontalp
+
+        if n_verticalp % 2:
+            n_max_slides += n_verticalp / 2
+        else:
+            n_max_slides += (n_verticalp - 1) / 2
+
+        n_slides = random.randint(1, n_max_slides)
+
+        initial_solution = Slideshow()
+
+        i = 1
+        for i in range(n_slides):
+            orientation = 0  # vertical slide by default
+
+            if (n_verticalp > 1 and n_horizontalp > 0):
+                orientation = random.randint(0, 1)
+            elif n_horizontalp > 0:
+                orientation = 1
+
+            if (orientation):  # horizontal slide
+                n = random.randint(1, n_horizontalp)
+
+                photo = Slideshow.horizontal_photos_pool[n]
+
+                # create slide
+                initial_solution.add_slide(Slide(photo))
+            else:  # vertical slide
+                # left photo
+                n = random.randint(1, n_verticalp)
+
+                left_photo = Slideshow.vertical_photos_pool[n]
+
+                n_verticalp -= 1
+
+                # right photo
+                n = random.randint(1, n_verticalp)
+
+                right_photo = Slideshow.vertical_photos_pool[n]
+
+                # create slide
+                initial_solution.add_slide(Slide(left_photo, right_photo))
+
+        return initial_solution
